12b01b0b3b1f0546376b9abb8bf252460e7f6a87
Chamara Dilshan <chamaradilshan0011@gmail.com>
2024-01-11 04:00:14 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+import { useNavigate } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const navigate = useNavigate();
+  
   return (
     <div className="App">
       <header className="App-header">
         <h2>Welcome To Chamara</h2>
-        <button className='users-button'>users</button>
+        <button className='users-button' onClick={() => navigate('/users')}>users</button>
         
       </header>
     </div>
--- src/Users.js
+++ src/Users.js
@@ -1,10 +1,34 @@
 import React from 'react'
 import UserForm from './UserForm'
+import UsersTable from './UsersTable'
+import { Box } from '@mui/material';
+
+
+const users = [
+  {
+    id:1,
+    name:'Chamara'
+  },
+  {
+    id:2,
+    name:'Dilshan'
+  },
+];
 
 const Users = () => {
   return (
     <div>
-        <UserForm/>
+        <Box
+          sx={{
+            width:'calc(100% - 200px)',
+            margin: 'auto',
+            marginTop:'100px',
+           
+          }}
+        >
+          <UserForm/>
+          <UsersTable rows={users}/>
+        </Box>
     </div>
   )
 }
